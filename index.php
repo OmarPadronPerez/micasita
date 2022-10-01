bd8a24da29bf925c8dc5db8a5d97a0ec2dc8c1d0
OmarPadronPerez <omar.padron.perez@gmail.com>
2022-10-01 12:58:38 -0500

    dsf

--- index.php
+++ index.php
@@ -18,9 +18,30 @@
           <div class="col-1">12</div>
         </div>
       <!--fin regla-->
+      <div class="col-12"> 
+      <table class="table">
+          <thead>
+            <tr>
+              <th>titulo1</th>
+              <th>titulo2</th>
+            </tr>
+          </thead>
+          <tr>
+            <!--tarjeta-->
+            <td class="td_checkbox"> <input type="checkbox" id="cbox1"> </td>
 
-
-
+            <td class="td_descripcion">
+                <div class="nombre">
+                  asdf
+                </div>
+                <div class="precio">
+                  $156 descripcion lo suficiente mente grande como para tener que usar varias filar
+                </div>
+            </td>
+            <!--tarjeta-->
+          </tr>
+        </table>
+      </div>
 
 
     </div><!--contenedor principal-->
