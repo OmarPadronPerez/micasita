2c3b4ac4afe86e4090f8d0c136f97ba290aa5004
OmarPadronPerez <omar.padron.perez@gmail.com>
2022-10-01 12:30:14 -0500

    primer commit

---
+++ index.php
@@ -0,0 +1,28 @@
+<?php include("template/cabecera.php"); ?>
+
+    <div class="container-fluid principal" > <!--contenedor principal-->
+        
+       <!--  Regla  -->
+       <div class="row regla" id="regla">
+          <div class="col-1">1</div>
+          <div class="col-1">2</div>
+          <div class="col-1">3</div>
+          <div class="col-1">4</div>
+          <div class="col-1">5</div>
+          <div class="col-1">6</div>
+          <div class="col-1">7</div>
+          <div class="col-1">8</div>
+          <div class="col-1">9</div>
+          <div class="col-1">10</div>
+          <div class="col-1">11</div>
+          <div class="col-1">12</div>
+        </div>
+      <!--fin regla-->
+
+
+
+
+
+    </div><!--contenedor principal-->
+    
+  <?php include("template/pie.php"); ?>
---
+++ template/cabecera.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <title>Mi Casita </title>
+    <link rel="icon" type="image/x-icon" href="imagenes/icono.ico">
+    <link rel="stylesheet" href="css/pruevas.css" />
+    <link rel="stylesheet" href="css/general.css" />
+    <!--bootstrap-->
+    <link 
+      href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css"
+      rel="stylesheet"
+      integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT"
+      crossorigin="anonymous"
+    />
+    <script
+      src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js"
+      integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3"
+      crossorigin="anonymous"
+    ></script>
+    <script
+      src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.min.js"
+      integrity="sha384-7VPbUDkoPSGFnVtYi0QogXtr74QeVeeIs99Qfg5YCF+TidwNdjvaKZX19NZ/e6oz"
+      crossorigin="anonymous"
+    ></script>
+    
+  </head>
+  <body>
+    <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark"><!--navegacion-->
+        <div class="container-fluid">
+          <a class="navbar-brand" href="#">Mi casita bonita</a>
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarCollapse">
+            <ul class="navbar-nav me-auto mb-2 mb-md-0">
+              <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="#">Carrito</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" aria-current="page" href="#">Compras</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" aria-current="page" href="#">Productos</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" aria-current="page" href="#">Tiendas</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" aria-current="page" href="#">Servicios</a>
+              </li>
+              <!--<li class="nav-item">
+                <a class="nav-link" href="#">Link</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link disabled">Disabled</a>
+              </li>-->
+            </ul>
+            
+          </div>
+        </div>
+      </nav><!--fin navegacion-->
+
+       
